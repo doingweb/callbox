62c294312f1919437270cbc75edac1a2168e200b
Chris Antes <chris@chrisdoingweb.com>
2015-06-19 23:14:08 -0700

    Refactoring app and routes. Installing test framework things.

--- app/app.js
+++ app/app.js
@@ -1,23 +1,21 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var swig = require('swig');
-
-var routes = require('./routes');
+var
+  express = require('express'),
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  swig = require('swig');
 
 var app = express();
-
-// view engine setup
-app.engine('swig', swig.renderFile)
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'swig');
 
 var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
+
+setupViewEngine();
+
+setupRoutes();
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
@@ -28,43 +26,52 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes.root);
-app.use('/users', routes.users);
-app.use('/callbox', routes.callbox);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
+forward404ToErrorHandler();
 
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err,
-            title: 'error'
-        });
-    });
+  setupDevelopmentErrorHandler();
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {},
-        title: 'error'
-    });
-});
-
+setupProductionErrorHandler();
 
 module.exports = app;
+
+
+function setupViewEngine() {
+  app.engine('swig', swig.renderFile)
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'swig');
+}
+
+function setupRoutes() {
+  app.use('/', require('./routes/root'));
+  app.use('/users', require('./routes/user'));
+  app.use('/callbox', require('./routes/callbox'));
+}
+
+function forward404ToErrorHandler() {
+  app.use(function(req, res, next) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+  });
+}
+
+function setupDevelopmentErrorHandler() {
+  setupErrorHandler();
+}
+
+function setupProductionErrorHandler() {
+  setupErrorHandler({});
+}
+
+function setupErrorHandler(errorDetails) {
+  app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+          message: err.message,
+          error: errorDetails || err,
+          title: 'error'
+      });
+  });
+}
--- app/routes/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  root: require('./root'),
-  users: require('./user'),
-  callbox: require('./callbox')
-};
